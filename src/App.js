82c17710fdc201aee3c3f4b1f4bb4c77c2e4e2bb
Mark Enfermo <markanthony.enfermo@gmail.com>
2023-12-19 00:08:12 +0800

    added sample

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import { Container } from "react-bootstrap";
+import { Table } from "react-bootstrap";
+import { Navbar, Nav } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Header = () => {
+  return (
+    <Navbar bg="dark" variant="dark" className="p-3">
+      <Navbar.Brand href="#home">Project in Accounting</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link href="#features">General Journal</Nav.Link>
+        <Nav.Link href="#features">General Ledger</Nav.Link>
+        <Nav.Link href="#features">Trial Balance</Nav.Link>
+        <Nav.Link href="#features">Worksheet</Nav.Link>
+        <Nav.Link href="#features">Financial Statements</Nav.Link>
+      </Nav>
+    </Navbar>
+  );
+ };
+
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <Header/>
+      <Container>
+        <h1>On Progress</h1>
+      </Container>
+    </>
   );
 }
 
